76361a8069f1acf5df6177119eee9f92f2b896f7
Ramzi Alsallaq <ramzi.alsallaq@gmail.com>
2020-08-12 10:47:54 -0500

    Delete addAnnotToFilteredHits_fromUniProtAccess.sh

--- bin/addAnnotToFilteredHits_fromUniProtAccess.sh
+++
@@ -1,5 +0,0 @@
-module load parallel
-module load python/2.7.15-rhel7
-input=`sed 1d hits_in_1729716_EW1H-0179_S86_L001.csv`
-#echo "$input" | parallel --block 100k --pipe python splitFile.py 
-echo "$input" | parallel  --block 10k --pipe python addAnnotToFilteredHits_fromUniProtAccess.py
